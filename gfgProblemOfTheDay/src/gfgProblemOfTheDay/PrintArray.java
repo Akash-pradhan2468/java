a18111758dbff21da5f8a03c334220aef16536df
Akash-pradhan2468 <akash2468pradhan@gmail.com>
2023-09-16 10:54:05 +0530

    First code

---
+++ gfgProblemOfTheDay/src/gfgProblemOfTheDay/PrintArray.java
@@ -0,0 +1,17 @@
+package gfgProblemOfTheDay;
+
+public class PrintArray {
+static void printarr(int arr[]) {
+	for(int i=0;i<arr.length;i++) {
+		System.out.print(arr[i]+" ");
+	}
+	System.out.println();
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[]= {1,2,3,4,5};
+printarr(arr);
+
+	}
+
+}
---
+++ gfgProblemOfTheDay/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module gfgProblemOfTheDay {
+}
